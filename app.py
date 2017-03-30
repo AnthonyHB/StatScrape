a5213899b0da6284e87012758bd4814def4dfa59
Anthony Benites <anthony.benites17@gmail.com>
2017-03-29 22:53:27 -0700

    loop through sites array and refactor variables

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ Output: Projected Montly Sales & Show Projected Monthly Car Count
 """
 
 from bs4 import BeautifulSoup as bsoup
-import requests as rq
-import datetime as dt
 import calendar as cd
+import datetime as dt
+import requests as rq
+
+
 
 password = input("Please enter the password:")
 
@@ -39,25 +41,36 @@ with rq.Session() as s:
 
 # Parse HTML data 
 soup = bsoup(r.content, "lxml")
-site = "01A"
 
-# Monthly Sales Total
-divSales = soup.find_all("div", {"data-trayid": "totalsales"})
-for div in divSales:
-    liSales = div.find_all("li", attrs={"data-site": site})
-    for li in liSales:
-        # Remove commas from monSales, and convert to int
-        num = li.get_text().replace(',', '')
-        monSales = int(num.replace('$',''))
-        salesProj = ( monSales / days ) * daysIM
-        print(site + " Monthly Sales to Date: ${:,}".format(round(monSales)) + "." + "Projected Sales: ${:,}".format(round(salesProj))) 
-        
-# Monthly Cars Total
-divCount = soup.find_all("div", {"data-trayid": "total"})
-for div in divCount:
-    liCount = div.find_all("li", attrs={"data-site": site})
-    for li in liCount:
-        monCars = int(li.get_text().replace(',', ''))
-        carsProj = ( monCars / days ) * daysIM
-        print(site + " Monthly Car Count to Date: {:,}".format(monCars) + "." + "Projected Car Count: {:,}".format(round(carsProj))) 
+# Turn all sites into an array
+sites = []
+divSites = soup.find_all("div", {"data-trayid": "site"})
+for div in divSites:
+    liSites = div.find_all("li")
+    for li in liSites:
+        sites.append(li.get_text())
+
+for site in sites:
+
+    # Monthly Sales Total
+    divSales = soup.find_all("div", {"data-trayid": "totalsales"})
+    for div in divSales:
+        liSales = div.find_all("li", attrs={"data-site": site})
+        for li in liSales:
+            # Remove $ and comma from monSales, and convert to int
+            monSales = int(li.get_text().replace('$', '').replace(',',''))
+            projSales = ( monSales / days ) * daysIM
+            print(site + " Sales to Date: ${:,}".format(round(monSales))) 
+            print(site + " Projected Sales: ${:,}".format(round(projSales)))
+
+    # Monthly Cars Total
+    divCount = soup.find_all("div", {"data-trayid": "total"})
+    for div in divCount:
+        liCount = div.find_all("li", attrs={"data-site": site})
+        for li in liCount:
+            # Remove comma from monCars, and convert to int
+            monCars = int(li.get_text().replace(',', ''))
+            projCars = ( monCars / days ) * daysIM
+            print(site + " Car Count to Date: {:,}".format(monCars))
+            print(site + " Projected Car Count: {:,}".format(round(projCars))) 
         
